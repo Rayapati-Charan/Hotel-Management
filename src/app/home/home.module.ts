759201c73630ab9a8d78c58532ec06aa369f84f9
R. Charan <charan.r@heraizen.com>
2024-03-27 21:11:38 +0530

    Header and footer added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from '../home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [LoginComponent, SignupComponent, HomeComponent],
+  declarations: [LoginComponent, SignupComponent, HomeComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
